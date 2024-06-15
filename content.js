450d5ef60035a0ce0d55718e213467dc65f787aa
selenehienclone <vaporflyb@gmail.com>
2024-06-15 14:37:48 -0400

    safemodify with title

--- content.js
+++ content.js
@@ -1,47 +1,96 @@
 // content.js
 
 // Function to modify the page's HTML
+// function editPageHTML() {
+//     let username = "glamour_loop999";
+//     let myFlair = "https://lichess1.org/assets/______2/flair/img/food-drink.ice.webp";
+//     let silverTrophy = "https://lichess1.org/assets/_z7cHiT/images/trophy/Big-Silver-Cup.png";
+//     let goldTrophy = "https://lichess1.org/assets/_z7cHiT/images/trophy/Fancy-Gold.png";
+//
+//     // const trophiesContainer = document.querySelector('.trophies');
+//     // if (trophiesContainer) {
+//     //   const trophyHTML = `
+//     //     <span class="trophy perf top10" title="Horde Top 10!">
+//     //       <img src="https://lichess1.org/assets/_z7cHiT/images/trophy/Big-Silver-Cup.png">
+//     //     </span>
+//     //     <span class="trophy perf top50" title="Atomic Top 50 player!">
+//     //       <img src="https://lichess1.org/assets/_z7cHiT/images/trophy/Fancy-Gold.png">
+//     //     </span>
+//     //   `;
+//     //   trophiesContainer.insertAdjacentHTML('beforeend', trophyHTML);
+//     // }
+//
+//
+//     if (window.location.href.indexOf(username) > -1) {
+//         let trophy1 =
+//             '<span class="trophy perf top10" title="Horde Top 10!">' +
+//             `<img src="${silverTrophy}"></span>`;
+//         let trophy2 =
+//             '<span class="trophy perf top50" title="Atomic Top 50 player!">' +
+//             `<img src="${goldTrophy}"></span>`;
+//
+//         let defaultTrophies = '<div class="trophies"></div>'
+//         let customTrophies = `<div class="trophies">${trophy1}${trophy2}</div>`;
+//
+//         document.body.innerHTML = document.body.innerHTML.replace(
+//             defaultTrophies,
+//             customTrophies
+//         );
+//     }
+//
+//         let defaultUsername =
+//             `<span class="online user-link" data-href="/@/${username}">` +
+//             `<i class="line"></i>${username}` +
+//             '<a href="/account/profile" title="Set your flair">' +
+//             `<img class="uflair" src="${myFlair}"></a></span>`;
+//
+//         let customUsername =
+//             `<span class="online user-link" data-href="/@/${username}">` +
+//             '<i class="line"></i>' +
+//             '<span class="utitle" title="International Master">IM</span>&nbsp;' +
+//             username +
+//             '<a href="/account/profile" title="Set your flair">' +
+//             `<img class="uflair" src="${myFlair}"></a></span>`;
+//
+//         document.body.innerHTML = document.body.innerHTML.replace(
+//             defaultUsername,
+//             customUsername
+//         );
+//     }
+// }
+
 function editPageHTML() {
     let username = "glamour_loop999";
     let myFlair = "https://lichess1.org/assets/______2/flair/img/food-drink.ice.webp";
-    let silverTrophy = "https://lichess1.org/assets/_z7cHiT/images/trophy/Big-Silver-Cup.png";
-    let goldTrophy = "https://lichess1.org/assets/_z7cHiT/images/trophy/Fancy-Gold.png";
 
     if (window.location.href.indexOf(username) > -1) {
-        let trophy1 =
-            '<span class="trophy perf top10" title="Horde Top 10!">' +
-            `<img src="${silverTrophy}"></span>`;
-        let trophy2 =
-            '<span class="trophy perf top50" title="Atomic Top 50 player!">' +
-            `<img src="${goldTrophy}"></span>`;
+        // Select the user link element
+        const userLink = document.querySelector(`.online.user-link[data-href*="/@/${username}"]`);
+        if (userLink) {
+            // Add the title
+            const titleElement = document.createElement('span');
+            titleElement.className = 'utitle';
+            titleElement.title = 'International Master';
+            titleElement.textContent = 'IM ';
 
-        let defaultTrophies = '<div class="trophies"></div>'
-        let customTrophies = `<div class="trophies">${trophy1}${trophy2}</div>`;
+            // Insert the title before the username
+            const lineElement = userLink.querySelector('.line');
+            lineElement.insertAdjacentElement('afterend', titleElement);
 
-        document.body.innerHTML = document.body.innerHTML.replace(
-            defaultTrophies,
-            customTrophies
-        );
+            // Update the flair image
+            const imgElement = userLink.querySelector('.uflair');
+            if (imgElement) {
+                imgElement.src = myFlair;
+            }
 
-        let defaultUsername =
-            `<span class="online user-link" data-href="/@/${username}">` +
-            `<i class="line"></i>${username}` +
-            '<a href="/account/profile" title="Set your flair">' +
-            `<img class="uflair" src="${myFlair}"></a></span>`;
-
-        let customUsername =
-            `<span class="online user-link" data-href="/@/${username}">` +
-            '<i class="line"></i>' +
-            '<span class="utitle" title="International Master">IM</span>&nbsp;' +
-            username +
-            '<a href="/account/profile" title="Set your flair">' +
-            `<img class="uflair" src="${myFlair}"></a></span>`;
-
-        document.body.innerHTML = document.body.innerHTML.replace(
-            defaultUsername,
-            customUsername
-        );
+            // Ensure the username remains unchanged after modification
+            const usernameTextNode = userLink.childNodes[2];
+            if (usernameTextNode.nodeType === Node.TEXT_NODE) {
+                usernameTextNode.textContent = username;
+            }
+        }
     }
 }
 
 editPageHTML();
+
